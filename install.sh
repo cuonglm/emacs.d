d75187c03aee4712e20ed0fa8bbc8b8b71177fab
LE Manh Cuong <manhcuongle@hotmail.com>
2015-07-25 23:28:50 +0700

    Refactoring + add supported shells

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
 if [ ! -e ~/.cask ]; then
-    echo "Cloning Cask repo"
-    git clone git@github.com:cask/cask.git ~/.cask
+  echo "Cloning Cask repo"
+  git clone git@github.com:cask/cask.git ~/.cask
 fi
 
-if ! grep -q "cask/bin" ~/.bashrc; then
-    echo "export PATH=\$HOME/.cask/bin:\$PATH" >> ~/.bashrc
+case $SHELL in
+   (*zsh) rcfile=~/.zshrc  ;;
+   (*ksh) rcfile=~/.kshrc  ;;
+  (*bash) rcfile=~/.bashrc ;;
+      (*) echo Not supported shell && exit 1 ;;
+esac
+
+if ! grep -q "cask/bin" "$rcfile"; then
+  printf 'export PATH=$HOME/.cask/bin:$PATH\n' >> "$rcfile"
 fi
 
 export PATH="$HOME/.cask/bin:$PATH"
